67fc4460272d58960306571a8e157dd8161fcbb6
Sonia Sieiro Díaz <soniasieirod@gmail.com>
2019-07-01 22:55:06 +0200

    bolas random

--- main.py
+++ main.py
@@ -1,8 +1,38 @@
 import pygame as pg
 from pygame.locals import *
 import sys
+import random
 
 _fps = 60
+
+class Ball(pg.Surface):
+    x = 0
+    y = 0
+    color = (255, 255, 255)
+    velocidad = 5
+    dirx = velocidad
+    diry = velocidad
+
+    def __init__(self):
+        pg.Surface.__init__(self, (16,16))
+        self.fill(self.color)
+
+    def setColor(self, color):
+        self.color = color
+        self.fill(self.color)
+
+    def avanza(self):
+        if self.x >= 800:
+            self.dirx = -self.velocidad
+        if self.x <= 0:
+            self.dirx = self.velocidad
+        if self.y >= 600:
+            self.diry = -self.velocidad
+        if self.y <= 0:
+            self.diry = self.velocidad
+
+        self.x += self.dirx
+        self.y += self.diry
 
 class Game:
     clock = pg.time.Clock()
@@ -11,10 +41,22 @@ class Game:
         self.size = (width, height)
         self.display = pg.display
         self.screen = self.display.set_mode(self.size)
+        self.screen.fill((60, 60, 60))
         self.display.set_caption('Mi juego')
 
+        self.balls = []
 
-    def start (self):
+        for i in range(random.randrange(5,15)):
+            b = Ball()
+            b.setColor((random.randrange(256), random.randrange(256), random.randrange(256)))
+            b.x = random.randrange(800)
+            b.y = random.randrange(600)
+            b.velocidad = random.randrange(10)
+
+            self.balls.append(b)
+
+
+    def start(self):
         while True:
             self.clock.tick(_fps)
 
@@ -23,8 +65,19 @@ class Game:
                     pg.quit()
                     sys.exit()
 
+            #Modifica la posición de ball
+            for ball in self.balls:
+                ball.avanza()
+
+            #Pintar los sprites en screen
+            self.screen.fill((60,60,60))
+
+            for ball in self.balls:
+                self.screen.blit(ball, (ball.x, ball.y))
+
             self.display.flip()
 
 if __name__ == '__main__':
     pg.init()
-    game = Game(800,600)
+    game = Game(800, 600)
+    game.start()
