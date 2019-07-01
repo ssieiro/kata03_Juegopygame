8ad82731fd09077340193e9102898e65cda28f4a
Sonia Sieiro Díaz <soniasieirod@gmail.com>
2019-07-01 23:46:24 +0200

    prueba para subir

--- main.py
+++ main.py
@@ -34,6 +34,17 @@ class Ball(pg.Surface):
         self.x += self.dirx
         self.y += self.diry
 
+    def comprobarChoque(self, candidata):
+        choqueX = candidata.x >= self.x and candidata.x <= self.x+16 or \
+            candidata.x+16 >= self.x and candidata.x+16 <= self.x+16
+        choqueY = candidata.y >= self.y and candidata.y <= self.y+16 or \
+            candidata.y+16 >= self.y and candidata.y+16 <= self.y+16
+        
+        if choqueX and choqueY:
+            self.dirx = self.dirx * -1
+
+        # Como es una condición devuelve True o False por lo que no hace falta hacer un if
+
 class Game:
     clock = pg.time.Clock()
 
@@ -44,17 +55,17 @@ class Game:
         self.screen.fill((60, 60, 60))
         self.display.set_caption('Mi juego')
 
-        self.balls = []
+        self.ball1 = Ball()
+        self.ball1.setColor((255,0,0))
+        self.ball1.x = random.randrange(800)
+        self.ball1.y = random.randrange(600)
+        self.ball1.velocidad = random.randrange(4, 9)
 
-        for i in range(random.randrange(5,15)):
-            b = Ball()
-            b.setColor((random.randrange(256), random.randrange(256), random.randrange(256)))
-            b.x = random.randrange(800)
-            b.y = random.randrange(600)
-            b.velocidad = random.randrange(10)
-
-            self.balls.append(b)
-
+        self.ball2 = Ball()
+        self.ball2.setColor((255,255,0))
+        self.ball2.x = random.randrange(800)
+        self.ball2.y = random.randrange(600)
+        self.ball2.velocidad = random.randrange(4, 9)
 
     def start(self):
         while True:
@@ -66,14 +77,19 @@ class Game:
                     sys.exit()
 
             #Modifica la posición de ball
-            for ball in self.balls:
-                ball.avanza()
+            self.ball1.avanza()
+            self.ball2.avanza()
+
+            self.ball1.comprobarChoque(self.ball2)
+            self.ball2.comprobarChoque(self.ball1)
 
             #Pintar los sprites en screen
             self.screen.fill((60,60,60))
 
-            for ball in self.balls:
-                self.screen.blit(ball, (ball.x, ball.y))
+            # blit coloca la bola en la pantalla en la posición que le demos
+
+            self.screen.blit(self.ball1, (self.ball1.x, self.ball1.y))
+            self.screen.blit(self.ball2, (self.ball2.x, self.ball2.y))
 
             self.display.flip()
 
