bdf8c8d1c9c5cfe6bd51b71a2d7ddb2c218c8bdb
Sonia Sieiro Díaz <soniasieirod@gmail.com>
2019-07-03 00:48:04 +0200

    primera versión final del juego

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import pygame as pg
 from pygame.locals import *
-import sys
+import sys, os
 import random
 
 _fps = 60
 
-class Ball(pg.Surface):
+def between(valor, liminf, limsup):
+    return liminf <= valor <= limsup
+
+class Raquet(pg.Surface):
     x = 0
     y = 0
+    w = 16
+    h = 96
     color = (255, 255, 255)
     velocidad = 5
-    dirx = velocidad
-    diry = velocidad
+    diry = 1
 
     def __init__(self):
-        pg.Surface.__init__(self, (16,16))
+        pg.Surface.__init__(self, (self.w, self.h))
         self.fill(self.color)
 
     def setColor(self, color):
@@ -22,31 +26,88 @@ class Ball(pg.Surface):
         self.fill(self.color)
 
     def avanza(self):
-        if self.x >= 800:
-            self.dirx = -self.velocidad
-        if self.x <= 0:
-            self.dirx = self.velocidad
-        if self.y >= 600:
-            self.diry = -self.velocidad
-        if self.y <= 0:
-            self.diry = self.velocidad
+        self.y += self.diry * self.velocidad
 
-        self.x += self.dirx
-        self.y += self.diry
+        if self.y <=0:
+            self.y = 0
+ 
+        if self.y >= 600 - self.h:
+            self.y = 600 - self.h
+
+
+class Ball(pg.Surface):
+    x = 0
+    y = 0
+    w = 16
+    h = 16
+    color = (255, 255, 255)
+    velocidad = 5
+    dirx = 1
+    diry = 1
+
+    def __init__(self):
+        pg.Surface.__init__(self, (self.w, self.h))
+        self.fill(self.color)
+
+        self.sound = pg.mixer.Sound(os.getcwd()+'/assets/sonido.aiff')
+
+    '''
+    def color(self, valor=None): #si le informamos de un color es un setter y si no un getter (nos da el valor actual) 
+        if valor == None:
+            return self.color
+        self.color = valor
+        self.fill(self.color)
+    '''
+    def setColor(self, color):
+        self.color = color
+        self.fill(self.color)
+
+    def saque(self, ganador):
+        self.x = 392
+        self.y = 292
+        self.diry = random.choice([-1,1])
+
+        if ganador == 1:
+            self.dirx = -1
+        else:
+            self.dirx = 1
+
+    def avanza(self):
+        if self.x >= 800:
+            self.saque(1)
+            return 2
+
+        if self.x <= 0:
+            self.saque(2)
+            return 1
+
+        if self.y >= 584:
+            self.diry = -1
+        if self.y <= 0:
+            self.diry = 1
+
+        self.x += self.dirx * self.velocidad
+        self.y += self.diry * self.velocidad
+
+        return None
 
     def comprobarChoque(self, candidata):
-        choqueX = candidata.x >= self.x and candidata.x <= self.x+16 or \
-            candidata.x+16 >= self.x and candidata.x+16 <= self.x+16
-        choqueY = candidata.y >= self.y and candidata.y <= self.y+16 or \
-            candidata.y+16 >= self.y and candidata.y+16 <= self.y+16
-        
-        if choqueX and choqueY:
-            self.dirx = self.dirx * -1
 
-        # Como es una condición devuelve True o False por lo que no hace falta hacer un if
+        if (between(self.y, candidata.y, candidata.y+candidata.h) or between(self.y+self.h, candidata.y, candidata.y+candidata.h)) and \
+           (between(self.x, candidata.x, candidata.x+candidata.w) or between(self.x+self.w, candidata.x, candidata.x+candidata.w)):
+            self.dirx = self.dirx * -1
+            self.x += self.dirx
+
+            self.sound.play()
+
+#Si la esquina superior de A coindice con cualquiera de las esquinas de B o viceversa, es que han chocado
 
 class Game:
     clock = pg.time.Clock()
+    pause = False
+    puntuaciones = {1: 0, 2: 0}
+    winScore = 3
+    winner = None
 
     def __init__(self, width, height):
         self.size = (width, height)
@@ -56,42 +117,145 @@ class Game:
         self.display.set_caption('Mi juego')
 
         self.ball1 = Ball()
-        self.ball1.setColor((255,0,0))
-        self.ball1.x = random.randrange(800)
-        self.ball1.y = random.randrange(600)
-        self.ball1.velocidad = random.randrange(4, 9)
+        self.ball1.color = (255, 0, 0)
+        self.ball1.setColor((255, 0, 0))
 
-        self.ball2 = Ball()
-        self.ball2.setColor((255,255,0))
-        self.ball2.x = random.randrange(800)
-        self.ball2.y = random.randrange(600)
-        self.ball2.velocidad = random.randrange(4, 9)
+        self.player1 = Raquet()
+
+        self.player2 = Raquet()
+
+        self.fuente = pg.font.Font(os.getcwd()+'/assets/font.ttf', 48)
+        self.iniciopartida()
+    
+    def gameover(self):
+        pg.quit()
+        sys.exit()
+
+    def iniciopartida(self):
+        self.ball1.x = 392
+        self.ball1.y = 292
+        self.ball1.diry = random.choice([-1,1])
+        self.ball1.dirx = random.choice([-1, 1])
+        self.ball1.velocidad = random.randrange(5, 11)
+
+        self.player1.x = 768
+        self.player1.y = 252
+
+        self.player2.y = 252
+        self.player2.x = 16
+
+        self.puntuaciones[1] = 0
+        self.puntuaciones[2] = 0
+
+        self.winner = None
+
+        self.marcador1 = self.fuente.render(str(self.puntuaciones[1]), 1, (255, 255, 255))
+        self.marcador2 = self.fuente.render(str(self.puntuaciones[2]), 1, (255, 255, 255))
+
+
+    def handleevent(self):
+        for event in pg.event.get():
+            if event.type == QUIT:
+                self.gameover() 
+
+            # Controlamos pulsaciones de teclas
+            if event.type == KEYDOWN:
+                if event.key == K_UP:
+                    self.player1.diry = -1
+                    self.player1.velocidad = 5
+                    self.player1.avanza()
+
+                if event.key == K_DOWN:
+                    self.player1.diry = 1
+                    self.player1.velocidad = 5
+                    self.player1.avanza()
+
+                if event.key == K_q:
+                    self.player2.diry = -1
+                    self.player2.velocidad = 5
+                    self.player2.avanza()
+
+                if event.key == K_a:
+                    self.player2.diry = 1
+                    self.player2.velocidad = 5
+                    self.player2.avanza()
+
+                if event.key == K_SPACE:
+                    if self.winner:
+                        self.iniciopartida()
+                    self.pause = False
+# Controlamos teclas mantenidas
+        keys_pressed = pg.key.get_pressed()
+        if keys_pressed[K_UP]: #get_pressed tiene valor 0 cuando no esta pulsado y valor 1 cuando sí
+            #devuelve false si es 0 y true si es 1 u otro valor
+            self.player1.diry = -1
+            if self.player1.velocidad < 15:
+                self.player1.velocidad += 1
+            self.player1.avanza()
+
+        if keys_pressed[K_DOWN]:
+            self.player1.diry = 1
+            if self.player1.velocidad < 15:
+                self.player1.velocidad += 1
+            self.player1.avanza()                
+
+        if keys_pressed[K_q]:
+            self.player2.diry = -1
+            if self.player2.velocidad < 15:
+                self.player2.velocidad += 1
+            self.player2.avanza()
+
+        if keys_pressed[K_a]:
+            self.player2.diry = 1
+            if self.player2.velocidad < 15:
+                self.player2.velocidad += 1
+            self.player2.avanza()      
+    def recalculate(self):
+        #Modifica la posición de ball y comprueba sus
+        if not self.pause: # en avanza de Ball devuelve 0(false) si no pasa nada y 1 o 2 si puntua alguien
+                #Si devuelve algo es que ha puntuado alguien por lo que para para volver a sacar
+            p = self.ball1.avanza()
+            if p:
+                self.pause = True #si p devuelve algo es True
+                self.puntuaciones[p] += 1
+                self.marcador1 = self.fuente.render(str(self.puntuaciones[1]), 1, (255, 255, 255))
+                self.marcador2 = self.fuente.render(str(self.puntuaciones[2]), 1, (255, 255, 255))
+
+                if self.puntuaciones[1] >= self.winScore or self.puntuaciones[2] >= self.winScore:
+                    self.winner = self.fuente.render("Ganador jugador {}".format(p), 1, (255, 255, 0))
+                    
+
+        self.ball1.comprobarChoque(self.player1)
+        self.ball1.comprobarChoque(self.player2)
+
+    def render(self):
+        #Pintar los sprites en screen
+        self.screen.fill((60,60,60))
+
+        self.screen.blit(self.ball1, (self.ball1.x, self.ball1.y))
+        self.screen.blit(self.player1, (self.player1.x, self.player1.y))
+        self.screen.blit(self.player2, (self.player2.x, self.player2.y))
+        self.screen.blit(self.marcador2, (32, 8))
+        self.screen.blit(self.marcador1, (720, 8))
+
+        if self.winner:
+            rect = self.winner.get_rect()
+            self.screen.blit(self.winner, ((800 - rect.w)//2, (600 - rect.h) // 2) )
+
+        
+
+        self.display.flip()  
 
     def start(self):
         while True:
             self.clock.tick(_fps)
 
-            for event in pg.event.get():
-                if event.type == QUIT:
-                    pg.quit()
-                    sys.exit()
+            self.handleevent()
 
-            #Modifica la posición de ball
-            self.ball1.avanza()
-            self.ball2.avanza()
+            self.recalculate()
 
-            self.ball1.comprobarChoque(self.ball2)
-            self.ball2.comprobarChoque(self.ball1)
+            self.render()  
 
-            #Pintar los sprites en screen
-            self.screen.fill((60,60,60))
-
-            # blit coloca la bola en la pantalla en la posición que le demos
-
-            self.screen.blit(self.ball1, (self.ball1.x, self.ball1.y))
-            self.screen.blit(self.ball2, (self.ball2.x, self.ball2.y))
-
-            self.display.flip()
 
 if __name__ == '__main__':
     pg.init()
